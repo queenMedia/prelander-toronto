7d27d5eb130a0383a1573595a09b035f65e83514
ronyderra <derrarony@gmail.com>
2023-06-25 14:49:47 +0300

    chrisitna

--- src/config.js
+++ src/config.js
@@ -3,13 +3,13 @@ import FORMULA from "./assets/images/formula.png";
 import LEQUOTIDIEN from "./assets/images/logo.png"
 
 export const LINK = "https://givemes.com/outgoing";
-export const CHARACTER = "Manuel Luís Goucha";
-export const CHARACTER_FIRSTNAME = "Manuel";
+export const CHARACTER = "Cristina Ferreira";
+export const CHARACTER_FIRSTNAME = "Cristina";
 export const OFFER_NAME = "BitGPT";
 export const MODAL = FORMULA;
 export const SUBTITLE_FIRST_LINE = `Slovenski državljani že kopičijo milijone evrov od doma z uporabo te "vrzeli bogastva" - ampak je to zakonito?`;
 export const SUBTITLE_SECOND_LINE = `est-ce légitime?`;
-export const FIRST_PARAGRAPH = `${CHARACTER}, conhecido como apresentador de televisão e jornalista português. Manuel ficou famoso por não se preocupar em ser honesto sobre como ganha dinheiro.`;
+export const FIRST_PARAGRAPH = `${CHARACTER}, conhecido como apresentador de televisão e jornalista português. ${CHARACTER_FIRSTNAME} ficou famoso por não se preocupar em ser honesto sobre como ganha dinheiro.`;
 export const LOGO = LEQUOTIDIEN;
 export const PAGE = "torontoOffer.com";
 
