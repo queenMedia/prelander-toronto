f8d9b84fac7db8d57eaea17f91bdc26db6336ade
ronyderra <derrarony@gmail.com>
2023-06-25 16:52:29 +0300

    Cristina_BitGPT

--- src/config.js
+++ src/config.js
@@ -1,3 +1,7 @@
+import Cristina_ContentImg1 from "../src/assets/images/img1.jpg";
+import Cristina_ContentImg2 from "../src/assets/images/img2.jpg";
+import Cristina_ContentImg3 from "../src/assets/images/img3.jpg";
+
 export const Cristina_BitGPT = {
   LINK: "https://givemes.com/outgoing",
   CHARACTER: "Cristina Ferreira",
@@ -5,4 +9,7 @@ export const Cristina_BitGPT = {
   OFFER_NAME: "BitGPT",
   FIRST_PARAGRAPH: `Cristina Ferreira, conhecido como apresentador de televisão e jornalista português. Cristina ficou famoso por não se preocupar em ser honesto sobre como ganha dinheiro.`,
   PAGE: "torontoOffer.com",
+  ContentImg1: Cristina_ContentImg1,
+  ContentImg2: Cristina_ContentImg2,
+  ContentImg3: Cristina_ContentImg3,
 };
