5cf479ce3d85149ad8cb6d9906b4d62ba394750b
ronyderra <derrarony@gmail.com>
2023-06-25 16:42:25 +0300

    Cristina_BitGPT

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 import FORMULA from "./assets/images/formula.png";
-import LEQUOTIDIEN from "./assets/images/logo.png";
 
 export const Cristina_BitGPT = {
   LINK: "https://givemes.com/outgoing",
@@ -10,6 +9,5 @@ export const Cristina_BitGPT = {
   SUBTITLE_FIRST_LINE: `Slovenski državljani že kopičijo milijone evrov od doma z uporabo te "vrzeli bogastva" - ampak je to zakonito?`,
   SUBTITLE_SECOND_LINE: `est-ce légitime?`,
   FIRST_PARAGRAPH: `Cristina Ferreira, conhecido como apresentador de televisão e jornalista português. Cristina ficou famoso por não se preocupar em ser honesto sobre como ganha dinheiro.`,
-  LOGO: LEQUOTIDIEN,
   PAGE: "torontoOffer.com",
 };
