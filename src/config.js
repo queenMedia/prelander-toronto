f3369b1ff545a2bc81af2525ebbdd735bbceaa4e
ronyderra <derrarony@gmail.com>
2023-06-25 16:07:34 +0300

    FIRST_PARAGRAPH

--- src/config.js
+++ src/config.js
@@ -20,7 +20,7 @@ export const Cristina_BitGPT = {
   MODAL: FORMULA,
   SUBTITLE_FIRST_LINE: `Slovenski državljani že kopičijo milijone evrov od doma z uporabo te "vrzeli bogastva" - ampak je to zakonito?`,
   SUBTITLE_SECOND_LINE: `est-ce légitime?`,
-  FIRST_PARAGRAPH: `${CHARACTER}, conhecido como apresentador de televisão e jornalista português. ${CHARACTER_FIRSTNAME} ficou famoso por não se preocupar em ser honesto sobre como ganha dinheiro.`,
+  FIRST_PARAGRAPH: `Cristina Ferreira, conhecido como apresentador de televisão e jornalista português. Cristina ficou famoso por não se preocupar em ser honesto sobre como ganha dinheiro.`,
   LOGO: LEQUOTIDIEN,
   PAGE: "torontoOffer.com",
 };
