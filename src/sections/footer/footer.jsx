ab36e980a250b7001aae57286295ea9bd728db7c
Ricardo Jimenez <ricardojimenez@pop-os.localdomain>
2023-06-28 12:18:08 -0600

    FEAT: New header and footer

--- src/sections/footer/footer.jsx
+++ src/sections/footer/footer.jsx
@@ -1,19 +1,36 @@
 import "./footer.css";
 import "./footer.responsive.css";
 import { TelegramRedirect } from "../../components/telegramRedirect/telegramRedirect";
+import Logo from "../../assets/images/new_logo_blue.jpeg";
 
 export const Footer = () => {
   return (
-    <footer>
-        <div className="footer-content">
-          <b>PLEASE READ THE TERMS AND CONDITIONS CAREFULLY AND AGREE TO THE FOLLOWING PURCHASE TERMS AND CONDITIONS BEFORE ORDERING:</b>
-          <br /> <br />
-          <b>Privacy policy</b>
-          <p>We are not affiliated in any way with any press release. All trademarks on this website, whether registered or not, are the property of their respective owners. The authors of this website are not sponsored by, or associated with any third party trademarks or their owners, nor do they represent them, their owners, their products or services. Please note that this website and the comments/response presented above are intended to serve as an example of what some people have achieved through this or those products. Their website and each page on this website is loosely based on a true story, but has been changed in many ways, including but not limited to the scope: stories, photos and comments. For this reason, this website, as well as any pages on this website, should not be taken literally, nor should it be treated as a story based on facts. Their website and the results described on it, although possible for some, should not be considered as results that can only be obtained by reproducing the methods used. I UNDERSTAND THAT THE WEBSITE CONTAINS EXAMPLES ILLUSTRATING WHAT CAN BE ACHIEVED WITH THIS/THESE PRODUCT(S) AND THAT THE STORYS/COMMENTS ABOVE DO NOT SHOULD BE TAKEN LITERALLY. Their website receives remuneration for clicks or purchase of products presented on this page.</p>
-          <br /> <br />
-          <b>IMPORTANT INFORMATION FOR CUSTOMERS</b>
-          <p>The term "sponsored article" The advertorial is a combination of advertisement and article. It is written in the form of an article as an independent press release when it actually advertises he a specific product or object of interest. Sponsored articles present true information, but are edited by the author, who is most often a company promoting its products, in such a way as to emphasize or to clarify certain elements and thus maintain the interest of the reader. A well-known example are magazines available on board aircraft that contain articles about the destinations of the airline. Because of being a sponsored article, I AGREE THAT THE INTERNET WEBSITE HAS EXAMPLES OF ILLUSTRATING THAT CAN BE ACHIEVED THROUGH THIS PROGRAM AND THAT THE STORY DESCRIBED ABOVE SHOULD NOT BE UNDERSTOOD LITERALLY. Their website receives payment for clicks or purchases of products presented on this site. Their program is not a job, but a development opportunity that can help them learn how to make money through entrepreneurial initiatives. Anyone who decides to buy any money-making program is not guaranteed to earn money because of the fact that they have purchased this program. People who think they "bought this program, so they automatically make money" are wrong. Since every kind of education depends on many variables, it is hard to judge what effect you can expect, but the people who bought this program have signed up for additional workshops and training in addition to it, and have used the knowledge they gained in growing markets, kept to their commitments, and continued to learn. If you follow the path that these people have followed, you can expect a general increase in knowledge in the selected area, but you should not expect any specific earnings. The average users who have read the initial material but have not enrolled in training, are not keeping their commitments and do not use the knowledge they have gained, usually do not make any money. Although the success of the person presented is real, their image and name have been changed to protect their identity. According to the concept of the sponsored article, the comments included in the comments section are a representation of typical comments and experiences which have been presented in the form of dialogue but they are not real entries on this website but have been created for illustrative purposes.</p>
-        </div>
+    <footer className="footer">
+      <div className="footer_container">
+        <img src={Logo} alt="Logo" className="footer_container_logo" />
+        <ul className="footer_list_rigth">
+          <div className="footer_list_rigth_sub_container">
+            <li className="footer_list_rigth_item">
+              Termos e Privacidade
+            </li>
+            <li className="footer_list_rigth_item">
+              Estatuto Editorial
+            </li>
+            <li className="footer_list_rigth_item">
+              Ficha Técnica
+            </li>
+            <li className="footer_list_rigth_item">
+              Publicidade
+            </li>
+            <li className="footer_list_rigth_item">
+              Contactos
+            </li>
+          </div>
+          <li className="footer_list_rigth_item_copy">
+            Jornal de Notícias, 2023 © Todos os direitos reservados
+          </li>
+        </ul>
+      </div>
     </footer>
   );
 };
--- src/sections/header/header.js
+++ src/sections/header/header.js
@@ -3,48 +3,61 @@ import { TelegramRedirect } from "../../components/telegramRedirect/telegramRedi
 import "./header.css";
 import "./header.responsive.css";
 
-import header1200 from "../../assets/images/header/header_1200-t.jpg"
-import header1050 from "../../assets/images/header/header_1050.jpg"
-import header900 from "../../assets/images/header/header_900.jpg"
-import header650 from "../../assets/images/header/header_650.jpg"
-import header450 from "../../assets/images/header/header_450.jpg"
-
-import header1200c from "../../assets/images/header/header_1200-c.jpg"
-import header1200b from "../../assets/images/header/header_1200-b.jpg"
+import Logo from "../../assets/images/new_logo.jpeg";
+import Search from "../../assets/icons/newHeader/search.png";
+import User from "../../assets/icons/newHeader/user.png";
+import Moon from "../../assets/icons/newHeader/moon.png";
+import Mobile from "../../assets/icons/newHeader/mobile_menu.png";
 
 export const Header = () => {
   return (
     <header className="header_section">
       <div className="header_container">
-        <div className="header_top">
-          <div className="header_top_container">
-            <div className="header_top_menu_1200">
-              <TelegramRedirect><img src={header1200} alt="" className="header_top_img"/></TelegramRedirect>
-            </div>
-            <div className="header_top_menu_1050">
-              <TelegramRedirect><img src={header1050} alt="" className="header_top_img"/></TelegramRedirect>
-            </div>
-            <div className="header_top_menu_900">
-              <TelegramRedirect><img src={header900} alt="" className="header_top_img"/></TelegramRedirect>
-            </div>
-            <div className="header_top_menu_650">
-              <TelegramRedirect><img src={header650} alt="" className="header_top_img"/></TelegramRedirect>
-            </div>
-            <div className="header_top_menu_450">
-              <TelegramRedirect><img src={header450} alt="" className="header_top_img"/></TelegramRedirect>
-            </div>
-          </div>
-        </div>
-        <div className="header_middle">
-          <div className="header_middle_container">
-            <TelegramRedirect><img src={header1200c} alt="" className="header_top_img"/></TelegramRedirect>
-          </div>
-        </div>
-        <div className="header_bottom">
-          <div className="header_bottom_container">
-            <TelegramRedirect><img src={header1200b} alt="" className="header_top_img"/></TelegramRedirect>
-          </div>
-        </div>
+        <ul className="header_list_left">
+          <li className="header_list_left_item_image">
+            <img src={Logo} className="header_container_logo" />
+          </li>
+          <li className="header_list_left_item">
+            <a>ÚLTIMAS</a>
+          </li>
+          <li className="header_list_left_item">
+            <a>MAIS VISTAS</a>
+            
+          </li>
+          <li className="header_list_left_item">
+            <a>JN DIRETO</a>
+          </li>
+        </ul>
+        <ul className="header_list_rigth">
+          <li className="header_list_rigth_item_hash_tag_container">
+            <a className="header_list_rigth_item_hash_tag">#guerra</a>
+          </li>
+          <li className="header_list_rigth_item_hash_tag_container">
+            <a className="header_list_rigth_item_hash_tag">#ataque ao mercado</a>
+          </li>
+          <li className="header_list_rigth_item_hash_tag_container">
+            <a className="header_list_rigth_item_hash_tag">#wagner</a>
+            
+          </li>
+          <li className="header_list_rigth_item_head_phones_container">
+            <a className="header_list_rigth_item_head_phones">OUVIR TSF</a>    
+          </li>
+          <li className="header_list_rigth_item_news_paper_container">
+            <a className="header_list_rigth_item_news_paper">LER JN</a>    
+          </li>
+          <li className="header_list_rigth_item_icon_container">
+            <img className="header_list_rigth_item_icons" src={Search} />    
+          </li>
+          <li className="header_list_rigth_item_icon_container">
+            <img className="header_list_rigth_item_icons" src={User} />    
+          </li>
+          <li className="header_list_rigth_item_icon_container">
+            <img className="header_list_rigth_item_icons" src={Moon} />    
+          </li>
+          <li className="header_list_rigth_item_mobile_menu">
+            <img className="header_list_rigth_item_icons" src={Mobile} />
+          </li>
+        </ul>
       </div>
     </header>
   );
